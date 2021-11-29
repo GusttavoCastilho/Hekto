c475fdd9da8533da2bcf5d5140237176f1d11b42
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-11-29 12:11:08 -0300

    feat: Create component Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import Link from 'next/link'
+
+import { FiSearch } from 'react-icons/fi'
+
+import styles from '../../styles/Header.module.scss'
+
+import Button from '../Button';
+import Input from '../Input';
+
 
 const Header = () => {
     return (
-        <h1>Hello World</h1>
+        <div className={styles.wrapper}>
+
+            {/* Title */}
+            <Link href="/" passHref>
+                <h1 className={styles.title}>Hekto</h1>
+            </Link>
+
+            {/* nav */}
+            <nav className={styles.nav}>
+                <Link href="/" passHref>
+                    <a>Home</a>
+                </Link>
+                <Link href="/pages" passHref>
+                    <a>Pages</a>
+                </Link>
+                <Link href="/products" passHref>
+                    <a>Products</a>
+                </Link>
+                <Link href="/blog" passHref>
+                    <a>Blog</a>
+                </Link>
+                <Link href="/shop" passHref>
+                    <a>Shop</a>
+                </Link>
+                <Link href="/contact" passHref>
+                    <a>Contact</a>
+                </Link>
+            </nav>
+
+            <div className={styles.search}>
+                <Input />
+                <Button size="small" icon={<FiSearch />} />
+            </div>
+        </div>
     )
 }
 
--- src/components/Header/test.tsx
+++ src/components/Header/test.tsx
@@ -3,9 +3,15 @@ import { render, screen } from '@testing-library/react'
 import Header from '.'
 
 describe('<Header />', () => {
-    it('should render a heading text', () => {
+    it('should render the menu', () => {
         render(<Header />)
 
-        expect(screen.getByRole('heading', { name: /hello world/i })).toBeInTheDocument()
+        expect(screen.getByRole('heading', { name: /hekto/i })).toBeInTheDocument()
+        expect(screen.getByRole('link', { name: /home/i })).toBeInTheDocument()
+        expect(screen.getByRole('link', { name: /pages/i })).toBeInTheDocument()
+        expect(screen.getByRole('link', { name: /products/i })).toBeInTheDocument()
+        expect(screen.getByRole('link', { name: /blog/i })).toBeInTheDocument()
+        expect(screen.getByRole('link', { name: /shop/i })).toBeInTheDocument()
+        expect(screen.getByRole('link', { name: /contact/i })).toBeInTheDocument()
     })
 })
