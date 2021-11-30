b11933f44242d88b1de32e463834c646c8452aaa
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-11-30 18:16:19 -0300

    chore: add mock from Banner component

---
+++ src/components/Banner/mock.ts
@@ -0,0 +1,6 @@
+export default {
+    short_description: "Best Furniture For Your Castle....",
+    title: "New Furniture Collection Trends in 2020",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscingin phasellus non in justo.",
+    buttonText: "Shop Now"
+}
