9af72023cb835b7d5fc1065323ed44aef0a95e1b
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-12-01 10:03:37 -0300

    fix: Button Props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,20 +1,20 @@
-import React from "react";
+import React, { ButtonHTMLAttributes } from "react";
 
-import styles from '../../styles/Button.module.scss'
+import styles from "../../styles/Button.module.scss";
 
 type ButtonProps = {
-    children?: React.ReactNode
-    size?: 'small' | 'medium'
-    icon?: React.ReactNode
-}
+  children?: React.ReactNode;
+  size?: "small" | "medium";
+  icon?: React.ReactNode;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({ children, size = 'medium', icon }: ButtonProps) => {
-    return (
-        <button className={styles[size]}>
-            {children && <label>{children}</label>}
-            {icon && icon}
-        </button>
-    )
-}
+const Button = ({ children, size = "medium", icon, ...rest }: ButtonProps) => {
+  return (
+    <button className={styles[size]} {...rest}>
+      {children && <label>{children}</label>}
+      {icon && icon}
+    </button>
+  );
+};
 
 export default Button;
